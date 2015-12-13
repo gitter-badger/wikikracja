c46bb7bb75ec0e499d1babac31792751167db584
soma115 <soma115@o2.pl>
2015-12-13 21:00:58 +0100

    polls application added. Tables created by Sasza.

---
+++ polls/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Act(models.Model):
+    act_title = models.CharField(max_length=200)
+    act_link = models.CharField(max_length=200)
+
+
+class PreChoice(models.Model):
+    act = models.ForeignKey(Act)
+    text = models.CharField(max_length=200)
+    count = models.IntegerField(default=0)
+
+
+class Choice(models.Model):
+    act = models.ForeignKey(Act)
+    text = models.CharField(max_length=200)
+    yes_count = models.IntegerField(default=0)
+    no_count = models.IntegerField(default=0)
+
+
+class EnvoyChoice(models.Model):
+    act = models.ForeignKey(Act)
+    text = models.CharField(max_length=200)
+    yes_count = models.IntegerField(default=0)
+    no_count = models.IntegerField(default=0)
